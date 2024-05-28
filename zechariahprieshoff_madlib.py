2cd5cac3ff0bd798f48e2fca5d4209444eb00f0e
zprieshoff <zprieshoff@gmail.com>
2024-05-28 17:32:34 -0400

    Add files via upload

---
+++ zechariahprieshoff_madlib.py
@@ -0,0 +1,33 @@
+#Zechariah Prieshoff
+#Mad Lib!
+#May 7th, 2024
+#This program will create a mad lib by using user input to fill in the blanks.
+
+print("Lets create a Mad Lib!")
+
+#Varibles used: royalty, name, weather, adjective, noun, action verb, animal, color, adverb, food, vehicle, verb.
+
+royalty = input("Choose: King, Queen, Prince, or Princess: ")
+name = input("Enter a name, Andy for example: ")
+weather = input("Enter a kind of weather ending in 'y', rainy for example: ")
+adjective = input("Enter an adjective, calm for example: ")
+noun = input("Enter a noun, book for example: ")
+actionVerb = input("Enter an action verb that ends in 'ing', running for example: ")
+animal = input("Enter a kind of animal, warthog for example: ")
+color = input("Enter a color, pink for example: ")
+adverb = input("Enter an adverb, never for example: ")
+food = input("Enter a kind of food, taco for example: ")
+vehicle = input("Enter a kind of vehicle, tricycle for example: ")
+verb = input("Enter a verb, jump for example: ")
+
+print("Greate answers! Here's your Mad Lib!")
+
+print(f"\nOnce upon a time, there lived a {royalty} named {name}.")
+print(f"It was a {weather} day, and {royalty} {name} was feeling very {adjective} today!")
+print(f"When out of no where, a dangerous {animal} appeared! And it was {actionVerb} {adverb}!")
+print(f"Frightened, {royalty} {name} quickly picked up a {noun} to fight off the ferocious {animal}.")
+print(f"But the mighty {animal} reacted quickly and {adverb} dodged the attack from {royalty} {name}.")
+print(f"Just when things were starting to look bad, the castles chef came to the rescue riding a {color} {vehicle}!")
+print(f"Riding off into the sunset, the chef offered {royalty} {name} a plate of {food}s.")
+
+print("\nThanks for playing!")
